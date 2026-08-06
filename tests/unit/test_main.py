6f4016cdf608015ae9bfd756fab0a39799df0d6b
PARTHASARATHI B <parthasarathi442004@gmail.com>
2026-08-06 13:01:19 +0530

    chore: bump version to 1.20.11 (share-a-job + per-user features release)

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -234,7 +234,7 @@ class TestVersionConsistency:
         """
         import interntrack
 
-        assert interntrack.__version__ == "1.20.9"
+        assert interntrack.__version__ == "1.20.11"
 
 
 class TestRateLimitConfig:
